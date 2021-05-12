54ba9b407b384e25f3c0a6afea90dbaf4cb1351a
UJS814 <jeffrey.compton@capitalone.com>
2021-05-12 11:22:48 -0400

    Version 2.0

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # notarizedCheck.sh
 
 ### Overview
-Finds apps and KEXTs on local Mac and creates a CSV with notarization status for each found item.  
+Finds apps and KEXTs on local Mac and creates a CSV with the following information for each found item.   
+- Notarization Status  
+- Codesign Status  
+- Universal Binary (M1 Native) Status  
+
 
 ### Warnings
-This tool is in no way shape or form designed to be run en masse accross your Mac fleet.  This is only designed to be run locally on a periodic basis to find out which apps and KEXTs are notarized.
+This tool is in no way shape or form designed to be run en masse across your Mac fleet.  This is only designed to be run locally on a periodic basis to find out which apps and KEXTs are notarized.
 
 ### Troubleshooting
 If when running the script you notice that all applications come back with `Notarized = False`, this may be that you don't have XCode installed for the use of `stapler` or you do but Terminal is set to use the Command Line Tools which doesn't include `stapler`.
@@ -12,7 +16,7 @@ If when running the script you notice that all applications come back with `Nota
 To fix this issue, run `sudo xcode-select -s /Applications/Xcode.app/Contents/Developer` in Terminal and then run the script, which will now properly reflect Notarization information.
 
 ### Credit
-Credit to numerous fellow mac admins who posted on slack/twitter/etc info on `stapler` command 
+Credit to numerous fellow mac admins who posted on slack/twitter/etc info on `stapler` command
 
 ### Usage
 - Be sure to have latest XCode with command line tools installed  
--- notarizedCheck.sh
+++ notarizedCheck.sh
@@ -3,24 +3,28 @@
 
 IFS=$'\n'
 
-if ! hash "$(which stapler)" > /dev/null 2>&1
-  then echo -e "\nYou need the latest XCode with stapler CLI to run this tool.\n" ; exit 0
-fi
 if ! hash "$(which codesign)" > /dev/null 2>&1
   then echo -e "\nYou need the latest XCode with codesign CLI to run this tool.\n" ; exit 0
 fi
 
 targetCSV=$(mktemp $HOME/Desktop/notarized.XXXX)
-echo -e "\nLooking for Applications and KEXTs"
+echo -e "\nLooking for Applications, System Extensions, and KEXTs"
 echo -e "CSV will be opened upon completion.\n"
-printf "Name,Notarized,Codesigned,Codesign Fail Reason,Kind,Path" >> ${targetCSV}
+printf "Name,Notarized,Notarized Fail Reason,Codesigned,Codesign Fail Reason,Kind,Architecture,Path" >> ${targetCSV}
 itemNumber=0
-totalToAnalyze=$(mdfind -count -onlyin /Applications -onlyin "/Library/Application Support" -onlyin /Library/Extensions -onlyin /System/Library/Extensions -onlyin "$HOME/Applications" -onlyin "$HOME/Library/Application Support" 'kMDItemKind == "*Application*" || kMDItemKind == "*Kernel Extension*"')
-for foundItem in $(mdfind -onlyin /Applications -onlyin "/Library/Application Support" -onlyin /Library/Extensions -onlyin /System/Library/Extensions -onlyin "$HOME/Applications" -onlyin "$HOME/Library/Application Support" 'kMDItemKind == "*Application*" || kMDItemKind == "*Kernel Extension*"')
+totalToAnalyze=$(mdfind -count -onlyin /Applications -onlyin "/Library" -onlyin "$HOME/Applications" -onlyin "$HOME/Library/Application Support" 'kMDItemKind == "*Application*" || kMDItemKind == "*System Extension*" || kMDItemKind == "*Kernel Extension*"')
+for foundItem in $(mdfind -onlyin /Applications -onlyin "/Library" -onlyin "$HOME/Applications" -onlyin "$HOME/Library/Application Support" 'kMDItemKind == "*Application*" || kMDItemKind == "*System Extension*" || kMDItemKind == "*Kernel Extension*"')
   do
     itemName=$(mdls -raw -name kMDItemDisplayName ${foundItem})
     itemPath=${foundItem}
     itemKind=$(mdls -raw -name kMDItemKind ${foundItem})
+    itemArchRaw=$(mdls -raw -name kMDItemExecutableArchitectures ${foundItem})
+    if echo "$itemArchRaw" | grep "arm64" > /dev/null 2>&1
+      then itemArch="Universal"
+    elif echo "$itemArchRaw" | grep "x86" > /dev/null 2>&1
+      then itemArch="Intel"
+    else itemArch="Other"
+    fi
     let "itemNumber = $itemNumber + 1"
     echo -e "\nAnalyzing $itemName ($itemNumber of $totalToAnalyze)"
     if codesign -v ${foundItem} > /dev/null 2>&1
@@ -31,13 +35,18 @@ for foundItem in $(mdfind -onlyin /Applications -onlyin "/Library/Application Su
       codesignStatus="FALSE"
       codesignFailReason="$(codesign -v ${foundItem} 2>&1 | head -1 | awk -F ":" '{print $2}' | tr -d ',')"
     fi
-    if stapler validate ${foundItem} > /dev/null 2>&1
-      then notarizeStatus="TRUE"
-    else notarizeStatus="FALSE"
+    if spctl --assess ${foundItem} > /dev/null 2>&1
+      then
+        notarizeStatus="TRUE"
+        notarizeFailReason="NA"
+    else
+      notarizeStatus="FALSE"
+      notarizeFailReason=$(spctl --assess ${foundItem} 2>&1 > /dev/null | awk -F ':' '{print $NF}')
     fi
     echo "Notarized = $notarizeStatus"
     echo "Codesigned = $codesignStatus"
-    printf "\n$itemName,$notarizeStatus,$codesignStatus,$codesignFailReason,$itemKind,$itemPath" >> $targetCSV
+    echo "Architecture = $itemArch"
+    printf "\n$itemName,$notarizeStatus,$notarizeFailReason,$codesignStatus,$codesignFailReason,$itemKind,$itemArch,$itemPath" >> $targetCSV
   done
 mv ${targetCSV} ${targetCSV}.csv
 echo "CSV is ${targetCSV}.csv"
